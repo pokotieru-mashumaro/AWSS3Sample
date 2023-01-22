7c54bc7534b890afe25ece7a69194d73b45524f9
s1f102200299 <s1f102200299@iniad.org>
2023-01-22 12:47:10 +0900

    Initial Commit

---
+++ AWSS3Sample/AWSS3SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSS3SampleApp.swift
+//  AWSS3Sample
+//
+//  Created by iniad on 2023/01/22.
+//
+
+import SwiftUI
+
+@main
+struct AWSS3SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
