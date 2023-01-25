f3b09e3fcfea822be21a6e0e9aa9fba175b6ca18
pokotieru-mashumaro <k827umeboshi@gmail.com>
2023-01-25 12:02:08 +0900

    色々

--- AWSS3Sample/LetIsCharts.swift
+++ AWSS3Sample/LetIsCharts.swift
@@ -32,16 +32,20 @@ struct LetIsCharts: View {
         reWeights(userId: "aaa", type: "現在", date: calendar.date(from: DateComponents(year: 2023, month: 1, day: 28))!, weight: 55.5),
         reWeights(userId: "aaa", type: "現在", date: calendar.date(from: DateComponents(year: 2023, month: 1, day: 29))!, weight: 55.3),
         reWeights(userId: "aaa", type: "現在", date: calendar.date(from: DateComponents(year: 2023, month: 1, day: 30))!, weight: 54.9),
-        reWeights(userId: "aaa", type: "目標", date: calendar.date(from: DateComponents(year: 2023, month: 1, day: 30))!, weight: 55.0),
+        
         reWeights(userId: "aaa", type: "現在", date: calendar.date(from: DateComponents(year: 2023, month: 1, day: 31))!, weight: 54.7),
+        reWeights(userId: "aaa", type: "目標", date: calendar.date(from: DateComponents(year: 2023, month: 1, day: 31))!, weight: 55.0),
        // reWeights(userId: "aaa", type: "現在", date: calendar.date(from: DateComponents(year: 2023, month: 2, day: 1))!, weight: 54.5),
 
     ]
-        
+    
+    @AppStorage("IdealGraphSelection") var idealGraphSelection: Bool = false
+    @AppStorage("ideal_weght") var hopeWeight: String = ""
+    
     @State var currentActiveItem: reWeights?
     @State var plotWidth: CGFloat = 0
     
-    let dateRange: ClosedRange<Date> = {
+    let weekRange: ClosedRange<Date> = {
         let calendar = Calendar.current
         let start = calendar.date(byAdding: .day, value: -7, to: Date())!
         let end = calendar.date(byAdding: .day, value: 0, to: Date())!
@@ -49,81 +53,104 @@ struct LetIsCharts: View {
     }()
     
     var body: some View {
+        VStack {
+            Button {
+                idealGraphSelection.toggle()
+            } label: {
+                Image(systemName: "repeat")
+            }
+
+            
             Chart(sampledata) { item in
-                if item.type == "現在" {
-                    LineMark(x: .value("Date", item.date), y: .value("Weight", item.weight))
-                        .foregroundStyle(by: .value("color", item.type))
-                }
-                if item.type == "目標" {
-                    LineMark(x: .value("Date", item.date), y: .value("Weight", item.weight))
-                        .lineStyle(.init(lineWidth: 1, miterLimit: 1, dash: [5], dashPhase: 5))
-                        .foregroundStyle(by: .value("color", item.type))
-                        
-                    
-                    PointMark(x: .value("Date", item.date), y: .value("Weight", item.weight))
-                        .foregroundStyle(by: .value("color", item.type))
-                }
+                    if item.type == "現在" {
+                        LineMark(x: .value("Date", item.date), y: .value("Weight", item.weight))
+                            .foregroundStyle(by: .value("color", item.type))
+                    }
                 
-                if let currentActiveItem, currentActiveItem.id == item.id {
-                    RuleMark(x: .value("日付", currentActiveItem.date))
-                        .foregroundStyle(.orange.opacity(0.6))
-                        .lineStyle(.init(lineWidth: 2, miterLimit: 2, dash: [2], dashPhase: 5))
-                        .annotation(position: .top) {
-                            VStack(alignment: .leading, spacing: 6) {
-                                Text("体重")
-                                    .font(.caption)
-                                    .foregroundColor(.gray)
-                                
-                                Text(String(format: "%.1f", currentActiveItem.weight))
-                                    .font(.title3.bold())
-                            }
-                            .padding(.horizontal, 10)
-                            .padding(.vertical, 4)
-                            .background {
-                                RoundedRectangle(cornerRadius: 6, style: .continuous)
-                                    .fill(.white.shadow(.drop(radius: 2)))
-                            }
+                    if item.type == "目標", idealGraphSelection{
+                        LineMark(x: .value("Date", item.date), y: .value("Weight", item.weight))
+                            .lineStyle(.init(lineWidth: 1, miterLimit: 1, dash: [5], dashPhase: 5))
+                            .foregroundStyle(by: .value("color", item.type))
+                            
+                        
+                        PointMark(x: .value("Date", item.date), y: .value("Weight", item.weight))
+                            .foregroundStyle(by: .value("color", item.type))
+                    }
+                
+                if !idealGraphSelection {
+                    RuleMark(y: .value("体重", Double(hopeWeight) ?? 55.0))
+                        .foregroundStyle(.gray.opacity(0.08))
+                        .lineStyle(.init(lineWidth: 2, miterLimit: 2, dash: [5], dashPhase: 5))
+                        .annotation(alignment: .leading) {
+                            Text("目標")
+                                .foregroundColor(.gray.opacity(0.1))
+                                .font(.caption)
                         }
                 }
-            }
-            .frame(height: 250)
-            .padding()
-            .chartForegroundStyleScale([
-                "現在": .orange, "目標": .gray.opacity(0.6)
-            ])
-            .chartXAxis {
-                AxisMarks(values: .stride(by: .day, count: 1)) { _ in
-                    AxisGridLine()
-                    AxisTick()
-                    AxisValueLabel(format: .dateTime.day())
-                }
-            }
-            .chartYScale(domain: .automatic(includesZero: false))
-            .chartOverlay(content: { proxy in
-                GeometryReader { innerProxy in
-                    Rectangle()
-                        .fill(.clear)
-                        .contentShape(Rectangle())
-                        .gesture(
-                            DragGesture()
-                                .onChanged{ value in
-                                    let location = value.location
-                                    if let date: Date = proxy.value(atX: location.x) {
-                                        let calendar = Calendar.current
-                                        let day = calendar.component(.day, from: date)
-                                        if let currentItem = sampledata.first(where: { item in
-                                            calendar.component(.day, from: item.date) == day
-                                        }) {
-                                            self.currentActiveItem = currentItem
-                                            self.plotWidth = proxy.plotAreaSize.width
-                                        }
-                                    }
-                                }.onEnded{ value in
-                                    currentActiveItem = nil
+ 
+                    
+                    if let currentActiveItem, currentActiveItem.id == item.id {
+                        RuleMark(x: .value("日付", currentActiveItem.date))
+                            .foregroundStyle(.orange.opacity(0.6))
+                            .lineStyle(.init(lineWidth: 2, miterLimit: 2, dash: [2], dashPhase: 5))
+                            .annotation(position: .top) {
+                                VStack(alignment: .leading, spacing: 6) {
+                                    Text("体重")
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                    
+                                    Text(String(format: "%.1f", currentActiveItem.weight))
+                                        .font(.title3.bold())
                                 }
-                        )
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 4)
+                                .background {
+                                    RoundedRectangle(cornerRadius: 6, style: .continuous)
+                                        .fill(.white.shadow(.drop(radius: 2)))
+                                }
+                            }
+                    }
                 }
+                .frame(height: 250)
+                .padding()
+                .chartForegroundStyleScale([
+                    "現在": .orange, "目標": .gray
+                ])
+                .chartXAxis {
+                    AxisMarks(values: .stride(by: .day, count: 1)) { _ in
+                        AxisGridLine()
+                        AxisTick()
+                        AxisValueLabel(format: .dateTime.day())
+                    }
+                }
+//                .chartXScale(domain: 0...max)
+                .chartYScale(domain: .automatic(includesZero: false))
+                .chartOverlay(content: { proxy in
+                    GeometryReader { innerProxy in
+                        Rectangle()
+                            .fill(.clear)
+                            .contentShape(Rectangle())
+                            .gesture(
+                                DragGesture()
+                                    .onChanged{ value in
+                                        let location = value.location
+                                        if let date: Date = proxy.value(atX: location.x) {
+                                            let calendar = Calendar.current
+                                            let day = calendar.component(.day, from: date)
+                                            if let currentItem = sampledata.first(where: { item in
+                                                calendar.component(.day, from: item.date) == day
+                                            }) {
+                                                self.currentActiveItem = currentItem
+                                                self.plotWidth = proxy.plotAreaSize.width
+                                            }
+                                        }
+                                    }.onEnded{ value in
+                                        currentActiveItem = nil
+                                    }
+                            )
+                    }
             })
+        }
     }
 }
 
